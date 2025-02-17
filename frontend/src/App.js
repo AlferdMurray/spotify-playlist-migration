e0cbe75f0dbe04ce2b7fb2fccf3c9f327f414cf5
AlferdMurray <alfredmurray3@gmail.com>
2025-02-18 01:41:46 +0530

    Replaced BrowerRout ot HashRout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,21 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 import ParentComponent from './Components/ParentComponent';
 import { Callback } from './Components/Callback';
-import { BrowserRouter as Router, Routes, Route,Navigate } from 'react-router-dom';
+// import { BrowserRouter as Router, Routes, Route,Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
   return (
     <Router>
-      {/* <nav>
-        <a href="/">Home</a> | <a href="/about">About</a> | <a href="/tokenAcquire">Acquire Token</a>
-      </nav> */}
       <Routes>
         <Route path="/spotify-playlist-migration" element={<ParentComponent />} />
-        <Route path='/Redirect' element={<Callback />} />
+        <Route path="/Redirect" element={<Callback />} />
         <Route path="*" element={<Navigate to="/spotify-playlist-migration" />} />
       </Routes>
     </Router>
   );
 }
+
 
 export default App;
